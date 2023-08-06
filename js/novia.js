7a54e8a0d52532f99b3ca2f81c24374cbe8dbbce
Néstor Eduardo Miranda Vega <59381371+neto2828@users.noreply.github.com>
2023-08-06 11:44:58 -0600

    cambio de JS

--- js/novia.js
+++ js/novia.js
@@ -1,31 +1,22 @@
-function mueveElBoton(){
-    const width = window.innerWidth;
-    const height = window.innerHeight;
+const button = document.getElementById("accionParaCuandoEllaDigaQuesi");
+button.addEventListener("click", () => {
+  const result = window.confirm("¡Ahora eres mi novia ❤️!");
+  if (result) {
+    window.location.href = "aceptada.html";
+  } else {
+    alert("Podrías volver a considerarlo 😔");
+  }
+});
 
-    const newWidth = Math.random() *width;
-    const newHeight = Math.random() *height;
+function mueveElBoton() {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
 
-    const btnNo = document.getElementById("btnNo");
-    btnNo.style.position = "absolute";
-    btnNo.style.left = newWidth + "px";
-    btnNo.style.top = newHeight + "px";
+  const newWidth = Math.random() * width;
+  const newHeight = Math.random() * height;
+
+  const btnNo = document.getElementById("btnNo");
+  btnNo.style.position = "absolute";
+  btnNo.style.left = newWidth + "px";
+  btnNo.style.top = newHeight + "px";
 }
-
-
-
-
-    const button = document.getElementById('accionParaCuandoEllaDigaQuesi');
-
-        // Agrega un evento click al botón
-        button.addEventListener('click', () => {
-            // Muestra la confirmación y captura la respuesta del usuario
-            const result = window.confirm('¡Ahora eres mi novia ❤️!');
-
-            // Si el usuario hace clic en "Aceptar", muestra un mensaje
-            if (result) {
-                window.location.href = 'aceptada.html';
-            } else {
-                alert('Podrías volver a considerarlo 😔');
-            }
-        });
-    
